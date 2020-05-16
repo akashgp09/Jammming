342d4ddca6441f9861260c8da52e79e07bf4d172
Akash Gupta <akashgp9@gmail.com>
2020-05-16 21:27:20 +0530

    Initial Commit :Added SearchResults

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import TrackList from "../TrackList/TrackList";
+import "./SearchResults.css";
+export default class SearchResults extends Component {
+  render() {
+    return (
+      <div className="SearchResults">
+        <h2>Results</h2>
+        <TrackList
+          tracks={this.props.searchResults}
+          onAdd={this.props.onAdd}
+          isRemoval={false}
+        />
+      </div>
+    );
+  }
+}
