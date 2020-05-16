e2ee0d4ab8dc3d420eee79b696f396e4361cd36c
Akash Gupta <akashgp9@gmail.com>
2020-05-16 21:27:40 +0530

    Initial Commit: Added Track

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import "./Track.css";
+export default class Track extends Component {
+  renderAction = () => {
+    if (this.props.isRemoval) {
+      return (
+        <button className="Track-action" onClick={this.removeTrack}>
+          -
+        </button>
+      );
+    }
+    return (
+      <button className="Track-action" onClick={this.addTrack}>
+        +
+      </button>
+    );
+  };
+  addTrack = () => {
+    this.props.onAdd();
+  };
+  removeTrack = () => {
+    this.props.onRemove(this.props.track);
+  };
+  render() {
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3>{this.props.track.name}</h3>
+          <p>
+            {this.props.track.artist} | {this.props.track.album}
+          </p>
+        </div>
+        {this.renderAction()}
+      </div>
+    );
+  }
+}
