7a655eec7b7e96e29e352744ee049f71b77e5487
Akash Gupta <akashgp9@gmail.com>
2020-05-17 20:22:35 +0530

    Added handleNameChange Method

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -2,10 +2,13 @@ import React, { Component } from "react";
 import TrackList from "../TrackList/TrackList";
 import "./Playlist.css";
 export default class Playlist extends Component {
+  handleNameChange = (event) => {
+    this.props.onNameChange(event.target.value);
+  };
   render() {
     return (
       <div className="Playlist">
-        <input defaultValue={"New Playlist"} />
+        <input defaultValue={"New Playlist"} onChange={this.handleNameChange} />
         <TrackList
           tracks={this.props.playlistTracks}
           onRemove={this.props.onRemove}
