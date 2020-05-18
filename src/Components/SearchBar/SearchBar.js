da90b0f2c73ced4dd93c6e0ef6fb5fe686dc976d
Akash Gupta <akashgp9@gmail.com>
2020-05-19 00:41:38 +0530

    Updated SearchBar Added onChange & onClick EventListener

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,11 +2,22 @@ import React, { Component } from "react";
 import "./SearchBar.css";
 
 export default class SearchBar extends Component {
+  search = () => {
+    this.props.onSearch(this.state.term);
+  };
+  handleTermChange = (e) => {
+    this.setState({ term: e.target.value });
+  };
   render() {
     return (
       <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" />
-        <button className="SearchButton">SEARCH</button>
+        <input
+          onChange={this.handleTermChange}
+          placeholder="Enter A Song, Album, or Artist"
+        />
+        <button onClick={this.search} className="SearchButton">
+          SEARCH
+        </button>
       </div>
     );
   }
